0258ee16efb62fe9e44af17c8a6ff94583a2cfd6
Etienne Michels <ebmichel>
2023-02-20 13:01:34 -0500

    Possibly working tiling + overlap

--- testTile.py
+++ testTile.py
@@ -1,13 +1,32 @@
 from PIL import Image
+import matplotlib 
+matplotlib.use('TKAgg')
 from matplotlib import pyplot as plt
-from tile import tile, untile
 from tiler import tiler
 import numpy as np
+
+
+
+
+a=np.zeros((32,32))
+c=1
+for i in range(0,32,4):
+    for j in range(0,32,4):
+        a[i:i+4,j:j+4]=c
+        c+=1
+
+t=tiler(a,4,4,2)
+t1=rt=t.untile(list(t))
+
+
+
+
+
 
 img = Image.open("img1.tif")
 img = np.array(img)
 
-tiles=tiler(img, 385, 385)
+tiles=tiler(img, 385, 385,2)
 reImg=tiles.untile(list(tiles))
 
 fig,ax = plt.subplots(5,4)
--- tiler.py
+++ tiler.py
@@ -1,13 +1,18 @@
 import numpy as np 
-
+from copy import deepcopy
 class tiler:
-    def __init__(self,img,n,m):
+    def __init__(self,img,n,m,overlap=0):
         self.img=img
         self._index=-1
         self.n=n
         self.m=m
+        self.nx=0
+        self.ny=0
+        self.xPadn=0
+        self.yPadn=0
+        self.overlap=overlap
         self.y,self.x=img.shape
-        self.tiles=self.tile(self.img,self.n, self.m)
+        self.tiles=self.tile(self.img,self.n, self.m,self.overlap)
 
     def __iter__(self):
         return self
@@ -19,20 +24,30 @@ class tiler:
         else:
             return self.tiles[self._index]
     def __list__(self):
-        return tiles
+        t=deepcopy(self.tiles)
+        return t
     def __getitem__(self,item):
         return self.tiles[item]
-    def tile(self,img,n,m):
+
+
+    
+    def tile(self,img,n,m,overlap=0):
         """
-        #Used to turn an image into tiles of size n x m where n is the number of pixels in the vertical, and m the horizontal
+        Used to turn an image into tiles of size n x m where n is the number of pixels in the vertical, and m the horizontal pixels
         """
         out=[]
+        T=[] #coord bounds of usable data
+        R=[] #true coord bounds of 
         
         if type(img)==list:
             img=np.array(img)
         y,x=img.shape
         xPadn=x%m
         yPadn=y%n
+
+        self.xPadn=xPadn
+        self.yPadn=yPadn
+
         if xPadn:
             xPadn+=-1
             xPad=np.zeros((y,xPadn))
@@ -48,9 +63,19 @@ class tiler:
         py,px=img.shape
         nx=int(px/m)
         ny=int(py/n)
-        for i in range(ny):
-            for j in range(nx):
-                sel=img[i*n:i*n+n,j*m:j*m+m]
+        self.nx=nx
+        self.ny=ny
+        #at this point, image is ready, & have number of tiles
+        if overlap:
+            oxPad=np.zeros((py,overlap))
+            img=np.hstack((oxPad,img,oxPad))
+
+            oyPad=np.zeros((overlap,px+overlap*2))
+            img=np.vstack((oyPad,img,oyPad))
+
+        for i in range(overlap,ny+overlap):
+            for j in range(overlap,nx+overlap):
+                sel=img[(i-overlap)*n:(i-overlap)*n+n+overlap*2,(j-overlap)*m:(j-overlap)*m+m+overlap*2]
                 out.append(sel)
 
 
@@ -62,38 +87,21 @@ class tiler:
         """
         y=self.y
         x=self.x
-        img=np.zeros((y,x))
-        n,m=tiles[0].shape
-        xPadn=x%m
-        yPadn=y%n
-        if xPadn:
-            xPadn+=-1
-            xPad=np.zeros((y,xPadn))
-            img=np.hstack((img,xPad))
-        if yPadn:
-            yPadn+=-1
-            if xPadn:
-                yPad=np.zeros((yPadn,x+xPadn))
-            else:
-                yPad=np.zeros((yPadn,x))
-            img=np.vstack((img,yPad))
-
-        nx=int((xPadn+x)/m)
-        ny=int((yPadn+y)/n)
+        nx=self.nx
+        ny=self.ny
+        n=self.n
+        m=self.m
+        overlap=self.overlap
+        img=np.zeros((y+self.yPadn,x+self.xPadn))
         c=0
         for i in range(ny):
             for j in range(nx):
-                img[i*n:i*n+n,j*m:j*m+m]=tiles[c]
+                img[i*n:i*n+n,j*m:j*m+m]=tiles[c][overlap:-overlap,overlap:-overlap]
                 c+=1
 
-        if xPadn:
-            img=img[:,0:-xPadn]
-        if yPadn:
-            img=img[0:-yPadn,:]
+        if self.xPadn:
+            img=img[:,0:-self.xPadn]
+        if self.yPadn:
+            img=img[0:-self.yPadn,:]
         return img
 
-
-if __name__ == '__main__':
-    c=retTest([1,2,3,4])
-    for i in c:
-        print(i)
